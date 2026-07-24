990f1e81a73095588702e0c936ed7927c36bf4ec
Roberto <roberto.teixeirafs@gmail.com>
2026-07-23 21:17:22 -0300

    Receive data from network remote input connection

--- Packages/InkBridgeNetworking/Sources/InkBridgeNetworking/NetworkRemoteInputConnection.swift
+++ Packages/InkBridgeNetworking/Sources/InkBridgeNetworking/NetworkRemoteInputConnection.swift
@@ -4,11 +4,16 @@ import Network
 @available(macOS 10.14, iOS 12.0, *)
 public final class NetworkRemoteInputConnection: RemoteInputConnection, @unchecked Sendable {
     private let connection: NWConnection
+    private let onData: ((Data) -> Void)?
 
     public private(set) var state: RemoteInputConnectionState = .disconnected
 
-    public init(connection: NWConnection) {
+    public init(
+        connection: NWConnection,
+        onData: ((Data) -> Void)? = nil
+    ) {
         self.connection = connection
+        self.onData = onData
     }
 
     public func start() {
@@ -19,6 +24,7 @@ public final class NetworkRemoteInputConnection: RemoteInputConnection, @uncheck
         }
 
         connection.start(queue: .main)
+        receiveNext()
     }
 
     public func stop() {
@@ -35,6 +41,29 @@ public final class NetworkRemoteInputConnection: RemoteInputConnection, @uncheck
                 }
             }
         )
+    }
+
+    private func receiveNext() {
+        connection.receive(
+            minimumIncompleteLength: 1,
+            maximumLength: 65_536
+        ) { [weak self] data, _, isComplete, error in
+            if let data, !data.isEmpty {
+                self?.onData?(data)
+            }
+
+            if let error {
+                self?.state = .failed(error.localizedDescription)
+                return
+            }
+
+            if isComplete {
+                self?.state = .disconnected
+                return
+            }
+
+            self?.receiveNext()
+        }
     }
 
     private func handle(_ nwState: NWConnection.State) {
